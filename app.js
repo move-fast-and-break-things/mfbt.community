0c8034b686020c5352b366d00fe2e5b596ecb743
Emma Maltseva <emmamaltseva0@gmail.com>
2023-07-13 23:00:07 +0300

    привет сайтик

---
+++ app.js
@@ -0,0 +1,12 @@
+function loadMore() {
+    var paragraphs = document.querySelectorAll('#projects_items .load_more');
+    for (var i = 0; i < paragraphs.length; i++) {
+      paragraphs[i].classList.add('show');
+    }
+    var hr = document.querySelectorAll('.line_hr');
+    for (var i = 0; i < hr.length; i++) {
+        hr[i].classList.add('sh');
+      }
+    document.getElementById('load-more-button').style.display = 'none';
+  }
+  
