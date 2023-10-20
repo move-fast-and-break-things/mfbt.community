975ba971e7f714267e9495a12abcb956ad03ecd5
PlutoniumSoap <alephnight.g@gmail.com>
2023-10-20 20:23:03 +0300

    Loading authors via GH api

--- app.js
+++ app.js
@@ -8,5 +8,32 @@ function loadMore() {
         hr[i].classList.add('sh');
       }
     document.getElementById('load-more-button').style.display = 'none';
-  }
-  
+}
+
+async function loadAuthors() {
+  const apiURL = "https://api.github.com/orgs/move-fast-and-break-things/members"
+  await fetch(apiURL)
+          .then((result) => result.json())
+          .then((data) => {
+            data.forEach(element => {
+              fetch(element.url)
+                .then((result) => result.json())
+                .then((data) => {
+                  document.querySelector("#creators > div > div").innerHTML +=
+                    `
+                    <div class="person">
+                      <div class="name_and_img">
+                        <img src="${data.avatar_url}" alt="">
+                        <p class="person_name">${data.name?data.name:data.login}</p>
+                      </div>
+                      <p class="about_person">${data.bio}</p>
+                    </div>
+                    `
+                })
+            })
+          })
+}
+
+window.onload = function() {
+  loadAuthors()
+}
