7c742d3cae21d3b2a9e7833dc16486ee33f5e573
chaitanya-bhargava <chaitanyabhargava5@gmail.com>
2024-10-03 17:26:57 +0530

    fix: replace var with let and const

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 function loadMore() {
-    var paragraphs = document.querySelectorAll('#projects_items .load_more');
-    for (var i = 0; i < paragraphs.length; i++) {
+    const paragraphs = document.querySelectorAll('#projects_items .load_more');
+    for (let i = 0; i < paragraphs.length; i++) {
       paragraphs[i].classList.add('show');
     }
-    var hr = document.querySelectorAll('.line_hr');
-    for (var i = 0; i < hr.length; i++) {
+    const hr = document.querySelectorAll('.line_hr');
+    for (let i = 0; i < hr.length; i++) {
         hr[i].classList.add('sh');
       }
     document.getElementById('load-more-button').style.display = 'none';
@@ -12,12 +12,12 @@ function loadMore() {
 }
 
 function hide() {
-    var paragraphs = document.querySelectorAll('#projects_items .load_more');
-    for (var i = 0; i < paragraphs.length; i++) {
+    const paragraphs = document.querySelectorAll('#projects_items .load_more');
+    for (let i = 0; i < paragraphs.length; i++) {
       paragraphs[i].classList.remove('show');
     }
-    var hr = document.querySelectorAll('.line_hr');
-    for (var i = 0; i < hr.length; i++) {
+    const hr = document.querySelectorAll('.line_hr');
+    for (let i = 0; i < hr.length; i++) {
         hr[i].classList.remove('sh');
       }
     document.getElementById('load-more-button').style.display = 'block';
